076e1ff56fa8fe18a034a4d72ca0f5460b4339c7
Sethukumar J <sethukumarj.76@gmail.com>
2023-02-15 07:37:12 -0500

    implemented RoomchatWebsocket handler

--- internal/handlers/chat.go
+++ internal/handlers/chat.go
@@ -1,1 +1,36 @@
 package handlers
+
+import (
+	"videochat/pkg/chat"
+
+	"github.com/gofiber/fiber"
+	"github.com/gofiber/websocket/v2"
+)
+
+func RoomChat(c *fiber.Ctx) error {
+	return c.Render("chat", fiber.Map{}, "layouts/main")
+}
+
+func RoomChatWebsocket(c *websocket.Conn) {
+
+	uuid := c.Params("uuid")
+	if uuid == "" {
+		return
+	}
+	w.RoomsLock.Lock()
+	room := w.Rooms[uuid]
+	w.RoomsLock.Unlock()
+	if room == nil {
+		return
+	}
+
+	if room.Hub == nil {
+		return
+	}
+
+	chat.PeerChatConn(c.Conn, room.Hub)
+}
+
+func StreamChatWebsocket(c *websocket.Conn) {
+
+}
