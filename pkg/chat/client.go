a53cd47b3dc629ddc73b4d7f300a971057bf2faa
Sethukumar J <sethukumarj.76@gmail.com>
2023-02-15 07:00:17 -0500

    implemented readPump, upgrader and Peerchat connection. Writepump is not implemented

--- pkg/chat/client.go
+++ pkg/chat/client.go
@@ -1,35 +1,71 @@
 package chat
 
-import( 
+import (
+	"bytes"
+	"log"
 	"time"
-	"github.com/fasthttp/websocket"
 
+	"github.com/fasthttp/websocket"
 )
 
-const(
-	writeWait = 10 * time.Second
-	pongWait = 60 * time.Second
-	pingPeriod = (pongWait *9)/10
+const (
+	writeWait      = 10 * time.Second
+	pongWait       = 60 * time.Second
+	pingPeriod     = (pongWait * 9) / 10
 	maxMessageSize = 512
 )
 
+var (
+	newline = []byte{'\n'}
+	space   = []byte{' '}
+)
 
 type Client struct {
-	Hub *Hub
+	Hub  *Hub
 	Conn *websocket.Conn
 	Send chan []byte
 }
 
+var upgrader = websocket.FastHTTPUpgrader{
+	ReadBufferSize:  1024,
+	WriteBufferSize: 1024,
+}
 
-func (c *Client) readPumb(){
+func (c *Client) readPumb() {
+	defer func() {
+		c.Hub.unregister <- c
+		c.Conn.Close()
+	}()
+
+	c.Conn.SetReadLimit(maxMessageSize)
+	c.Conn.SetReadDeadline(time.Now().Add(pongWait))
+	c.Conn.SetPongHandler(func(string) error {
+		c.Conn.SetReadDeadline(time.Now().Add(pongWait))
+		return nil
+	})
+
+	for {
+		_, message, err := c.Conn.ReadMessage()
+		if err != nil {
+			if websocket.IsUnexpectedCloseError(err, websocket.CloseGoingAway, websocket.CloseAbnormalClosure) {
+				log.Printf("error: %v", err)
+			}
+			break
+		}
+		message = bytes.TrimSpace(bytes.Replace(message, newline, space, -1))
+		c.Hub.broadcast <- message
+	}
+}
+
+func (c *Client) writePump() {
 
 }
 
-func (c *Client) writePump(){
+func PeerChatConn(c *websocket.Conn, hub *Hub) {
 
-}
+	client := &Client{Hub: hub, Conn: c, Send: make(chan []byte, 256)}
+	client.Hub.register <- client
 
-
-func PeerChatConn(){
-	
+	go client.writePump()
+	client.readPumb()
 }
